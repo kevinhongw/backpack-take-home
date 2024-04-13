9cf630a3126ad042255edb88be395fb3c9f421be
Kevin Hong <hong.w.kevin@gmail.com>
2024-04-13 13:20:00 -0700

    route change

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,13 +7,11 @@ const BANK_ACCOUNT_ID = "91f371a0-b370-4342-9907-cf26c37d16e2"
 
 export const router = createBrowserRouter([
   {
-    path: `/${URL_PREFIX}`,
-    element: (
-      <Navigate replace to={`${URL_PREFIX}/bank_accounts/${BANK_ACCOUNT_ID}`} />
-    ),
+    path: `/`,
+    element: <Navigate replace to={`/bank_accounts/${BANK_ACCOUNT_ID}`} />,
   },
   {
-    path: `/${URL_PREFIX}/bank_accounts/:bankAccountId`,
+    path: `/bank_accounts/:bankAccountId`,
     element: <BankAccountPage />,
   },
 ])
